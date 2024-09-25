c48417d4ec17d0c203343b4f34e3d7c0c03b188a
mohammed <mohamadzat6@gmail.com>
2024-09-25 18:04:07 +0200

    challenge

---
+++ challenge 07.c
@@ -0,0 +1,15 @@
+//challenge 07
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int n ;
+  printf("enter number : ");
+  scanf("%d",&n);
+  if(n%2 == 0)
+  printf("pair");
+  else
+  printf("impair");
+
+   
+
+}
